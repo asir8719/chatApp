ae275d7f4e0e0f9179cb35bb534f924ea6bf8937
MD Asir Khan <160753754+asir8719@users.noreply.github.com>
2025-08-18 14:31:35 +0530

    Update router.js

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,9 +1,7 @@
 import express from 'express'
 import { chatbot } from '../controller/chatBot-controller.js'
-import chat from '../controller/chat-Controller.js'
 const router = express.Router()
 
 router.route('/wassupai').post(chatbot)
-router.route('/chat').post(chat)
 
 export {router}
